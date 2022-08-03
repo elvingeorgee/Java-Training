6c3a3f4aff05e5a7b270dc52e7e65619279e3ee1
elvingeorgee <108898071+elvingeorgee@users.noreply.github.com>
2022-08-03 17:12:44 -0400

    Initial commit of training files
    I've been completing practice projects as I've started my Java learning.

---
+++ Training/src/Application.java
@@ -0,0 +1,105 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+       //String name = "BINGBONG";
+           //switch(name){
+               //case("Psych"):
+                   //System.out.println("The best in town");
+                   //break;
+               //case("Math"):
+                   //System.out.println("Too much brain work");
+                   //break;
+               //case("History"):
+                   //System.out.println("Hit the pillows");
+                   //break;
+               //case("Science"):
+                   //System.out.println("Degrom is back baby");
+                   //break;
+               //default:
+                   //System.out.println("Degrom good luck sweetie");
+
+
+
+           //}
+
+     //Scanner scanner = new Scanner(System.in);
+
+       // System.out.println("You are playing a game! Press Q or q to cut");
+       // String name = scanner.nextLine();
+
+        //if(name.equals("q") || name.equals("Q")){
+        //    System.out.println("You weren't good enough");
+        //}else{
+        //    System.out.println("U ain't going anywhere");
+        //}
+
+            //Scanner scanner = new Scanner(System.in);
+            //String name = "";
+
+            //while(name.isBlank()){
+                //System.out.println("Enter your name here:");
+                //name = scanner.nextLine();
+            //}
+
+        //System.out.println("Hello " +name);
+
+
+       //for(int i=10;i>=0;i--){
+           //System.out.println(i);
+        //}
+        //
+        //System.out.println("Mr.Reverse");
+
+
+ //Scanner scanner = new Scanner(System.in);
+
+ //int rows;
+ //int columns;
+ //String name = "";
+
+        //System.out.println("Enter how many rows u wan: ");
+        //rows = scanner.nextInt();
+        //System.out.println("Enter how many columns u wan: ");
+        //columns = scanner.nextInt();
+        //System.out.println("Enter wha symbol u wanna use: ");
+        //name = scanner.next();
+
+        //for(int i=1; i<=rows; i++){
+        //System.out.println();
+        //for(int j=1; j<=columns;j++) {
+        //    System.out.print(name);
+        //}
+
+        //}
+
+        //String[] cars = new String[3];
+
+        //cars[0] = "Lambo";
+        //cars[1] = "Toyota";
+        //cars[2] = "Altima";
+
+        //for(int i=0;i<= cars.length; i++){
+        //    System.out.println(cars[i]);
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Training2/src/Application.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        //String[][] cars = {
+            //    {"Pepperoni", "Ice Cream", "Corn"},
+           //     {"Salt","Pep", "Spice"},
+          //      {"Sweet","Sour","Sauce"}};
+
+        //for(int i=0; i <cars.length; i++){
+            //System.out.println();
+            //for(int j=0; j <cars[i].length; j++){
+          //      System.out.print(cars[i][j] + "");
+          //  }
+
+
+        //}
+
+        String name = "Bro";
+
+        //boolean result = name.equals("Bro");
+        //int result = name.length();
+        //char result = name.charAt(0);
+        //int result = name.indexOf("o");
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        String result = name.replace('o', 'a');
+
+        System.out.println(result);
+
+
+
+
+    }
+}
